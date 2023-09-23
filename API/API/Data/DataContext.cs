7e633d56dde1dbbb5537c0714651d1ab4fa2dc59
eugene <eugene.j.leroux@gmail.com>
2023-09-23 09:20:46 +0200

    added api code

---
+++ API/API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class DataContext : Microsoft.EntityFrameworkCore.DbContext
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
+        
+    }
+
+    public DbSet<AppUser> Users { get; set; }
+}
