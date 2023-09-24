e7c586626dbd8eda8fc635c35752183c3806a49a
eugene <eugene.j.leroux@gmail.com>
2023-09-24 10:29:20 +0200

    Added UI

--- API/API/Entities/AppUser.cs
+++ API/API/Entities/AppUser.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace API.Entities;
 
 public class  AppUser
--- API/API/Program.cs
+++ API/API/Program.cs
@@ -1,5 +1,9 @@
 using API.Data;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +15,8 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnectionString"));
 });
+
+builder.Services.AddCors();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -28,6 +34,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors(action => action.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
