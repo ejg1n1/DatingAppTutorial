f2a0f6c592edc62d01ce538f5de24e12981c1738
eugene <eugene.j.leroux@gmail.com>
2023-10-07 13:57:31 +0200

    finish member styling + add introfield to db

--- API/API/DTOs/MemberDto.cs
+++ API/API/DTOs/MemberDto.cs
@@ -15,6 +15,7 @@ public class MemberDto
     public string PhotoUrl { get; set; }    
     public string Gender { get; set; }
     public string LookingFor { get; set; }
+    public string Introduction { get; set; }
     public string Interests { get; set; }
     public string City { get; set; } 
     public string Country { get; set; }
